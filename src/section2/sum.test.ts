9082b289911cca347e6376231c33023fa99f40ef
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-01-07 16:35:01 +0900

    2-14 it.each

--- src/section2/sum.test.ts
+++ src/section2/sum.test.ts
@@ -1,5 +1,17 @@
 import { sum } from "./sum";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+// test("adds 1 + -2 to equal -1", () => {
+//   expect(sum(1, -2)).toBe(-1);
+// });
+
+it.each`
+  a    | b     | expected
+  ${1} | ${2}  | ${3}
+  ${1} | ${-2} | ${-1}
+`("sum($a, $b) should be $expected", ({ a, b, expected }) => {
+  expect(sum(a, b)).toBe(expected);
 });
